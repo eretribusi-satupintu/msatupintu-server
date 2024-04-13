6b6fe65359392ec5dd92caaef0abd5c053745346
Rizki24-star <kikingar.567@gmail.com>
2024-04-13 16:14:59 +0700

    connected prisma to niagahoster database

--- src/routes/authenticationRoutes.ts
+++ src/routes/authenticationRoutes.ts
@@ -1,12 +1,16 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import { register, login as loginController, logout } from '../controllers/authenticationController';
-import authValidation from '../middleware/authValidationMiddleware';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import {
+  register,
+  login as loginController,
+  logout,
+} from "../controllers/authenticationController";
+import authValidation from "../middleware/authValidationMiddleware";
 const router = express.Router();
 
 router.use(bodyParser.json());
 
-router.post('/register', async (req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
   try {
     const data = await register(req.body);
 
@@ -19,16 +23,17 @@ router.post('/register', async (req: Request, res: Response) => {
   }
 });
 
-router.post('/login', async (req: Request, res: Response) => {
+router.post("/login", async (req: Request, res: Response) => {
   try {
     const data = await loginController(req.body);
     res.status(200).json(data);
   } catch (error) {
+    console.log(error);
     res.status(403).json(error);
   }
 });
 
-router.post('/logout', authValidation, async (req: Request, res: Response) => {
+router.post("/logout", authValidation, async (req: Request, res: Response) => {
   try {
     const data = await logout();
     res.status(200).json(data);
